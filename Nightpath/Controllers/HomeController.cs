7f8474368f3ca0cf908e2a53c20e384e64fdf728
diogocosta95 <dogas.costa@gmail.com>
2015-11-04 22:37:15 +0000

    Page Points
    falta label do pontos, jumbotron dos pagamentos

--- Nightpath/Controllers/HomeController.cs
+++ Nightpath/Controllers/HomeController.cs
@@ -26,5 +26,12 @@ namespace Nightpath.Controllers
 
             return View();
         }
+
+        public ActionResult Points()
+        {
+            ViewBag.Message = "Manage your points.";
+
+            return View();
+        }
     }
 }
