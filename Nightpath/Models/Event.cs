75a28ddd8fe29e970c5384b6d30d435740cc938d
rmaoliveira <rmaoliveira@ua.pt>
2015-11-04 22:32:36 +0000

    Classes 90%
    Falta confirmar icollections

---
+++ Nightpath/Models/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nightpath.Models
+{
+    public class Event
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Capacity { get; set; }
+        public int EstablishmentID { get; set; }
+
+        public virtual Establishment Establishment { get; set; }
+    }
+}
---
+++ Nightpath/Models/Points.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nightpath.Models
+{
+    public class Points
+    {
+        public int EstablishmentID { get; set; }
+        public int ClientID { get; set; }
+        public int PointsNumber { get; set; }
+
+        public virtual Establishment Establishment { get; set; }
+        public virtual Client Client { get; set; }
+
+    }
+}
