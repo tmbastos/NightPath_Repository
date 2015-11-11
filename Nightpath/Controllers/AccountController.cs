271d17774ee50213cbf9726c34cc78bfbaea7300
Tiago <tiago_908@hotmail.com>
2015-11-11 21:17:58 +0000

    Update Links
    Login, Register, Feed

--- Nightpath/Controllers/AccountController.cs
+++ Nightpath/Controllers/AccountController.cs
@@ -102,7 +102,7 @@ namespace Nightpath.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Feed");
                 }
                 else
                 {
@@ -140,6 +140,11 @@ namespace Nightpath.Controllers
         // GET: /Account/ForgotPassword
         [AllowAnonymous]
         public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public ActionResult Feed()
         {
             return View();
         }
